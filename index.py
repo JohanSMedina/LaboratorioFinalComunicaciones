dc37320f15132fdcce1ad814d5bf5585e4c1536a
Johan <est.johan.medina@unimilitar.edu.co>
2022-05-16 21:41:08 -0500

    otra prueba

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 # .\lab\Scripts\activate
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 def home ():
     return render_template('tienda.html')
 
