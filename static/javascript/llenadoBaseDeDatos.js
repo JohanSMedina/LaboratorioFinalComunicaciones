e82a275d95eaae91e682b4c62e21f209561b1982
Johan <est.johan.medina@unimilitar.edu.co>
2022-05-17 23:45:41 -0500

    La cantidad de productos, pero va a tocar volver a cambiarla porque no vi el video qeu era maldita sea

--- static/javascript/llenadoBaseDeDatos.js
+++ static/javascript/llenadoBaseDeDatos.js
@@ -1,3 +1,7 @@
 //var conChocolatina = document.getElementById("conChocolatina");
 //conChocolatina.innerText = "Prueba de JS";
+document.getElementById('b2Chocolatina').addEventListener('submit',function(){
+alert('prueba')
+
+})
 
